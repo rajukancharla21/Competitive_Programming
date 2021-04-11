99efa3c8e7583e8cffc758b5782773a40ec21b47
Indi <indira.m@deltax.com>
2021-04-12 00:45:25 +0530

    stack quqes problems added

--- C++/basic_problems/stack_queue.cpp
+++ C++/basic_problems/stack_queue.cpp
@@ -1,5 +1,14 @@
 /*
 Q1- Given a string of charcters like (){}[] check whether string is valid or not
+Q2- Print just greater next value of each element
+Ex:
+IP: 4 5 2 25
+OP: 5 25 25 -1
+Q3- Print first Non-repeating chracter
+Ex: 
+IP: abaccc
+OP: b
+
 */
 #include <bits/stdc++.h>
 #include<iostream>
@@ -29,8 +38,53 @@ bool q1(string s){
     }
     if(st.empty()){cout<<"Valid";}
     else{cout<<"Invalid";}
+}
+void q2(vector<int> v, int n){
+    stack <int> st;
+    vector<int> res;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && st.top()<=v[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            res.push_back(-1);
+        }
+        else{
+            res.push_back(st.top());
+        }
+        st.push(v[i]);
+        //cout<<"Current Top"<<st.top()<<endl;
+    }
+    for(int i=n-1;i>=0;i--){
+        cout<<res[i]<<" ";
+    }
+
+}
+void q3(string ip){
+    queue<char> q;
+    int ar[26]={0};
+    for(int i=0;i<ip.length();i++){
+        if(ar[ip[i]-97]==0){
+            q.push(ip[i]);
+            ar[ip[i]-97]+=1;
+        }
+        else if(ar[ip[i]-97]==1){
+            q.pop();
+            ar[ip[i]-97]+=1;
+        }
+        else{
+            ar[ip[i]-97]+=1;
+        }
+    }
+    if(q.size()){
+        cout<<q.front();
+    }else{cout<<-1;}
 
 }
 int main(){
     //q1("{[]()}");
+    // vector <int> v={4,5,2,25};
+    // int n=4;
+    // q2(v,n);
+    q3("ababac");
 }
